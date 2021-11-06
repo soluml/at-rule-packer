40749223831f2fb9015829a563c87cb287b19b6c
soluml <benjamin@soluml.com>
2021-11-06 11:38:54 -0500

    Plugin seems like it's in good shape

--- test/postcss.js
+++ test/postcss.js
@@ -9,8 +9,8 @@ describe('postcss', () => {
     const css = await fs.promises.readFile(from, 'utf8');
     const result = await postcss([atRulePackerPlugin]).process(css, {from});
 
-    expect(result.css.trim()).toBe(
-      '@supports not (display:grid){main{float:right}.grid{display:flex}}'
+    expect(result.css.replace(/\s/g, '')).toBe(
+      '.mydiv{color:blue;font-size:1em;font-weight:bold;}/*Utilities*/.font-size--medium{font-size:1em;}.aspect-ratio--video{aspect-ratio:16/9;}@media(min-width:64em){.mydiv{font-size:1.25em;}.aspect-ratio--video{aspect-ratio:4/3;}}'
     );
   });
 });
